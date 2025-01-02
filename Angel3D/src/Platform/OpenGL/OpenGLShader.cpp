661e7241625f59e280f74e5e9259c38cc3345f43
gowthamkumar12 <gudhey.gowtham2002@gmail.com>
2025-01-02 16:23:27 +0530

    Added file reading check

--- Angel3D/src/Platform/OpenGL/OpenGLShader.cpp
+++ Angel3D/src/Platform/OpenGL/OpenGLShader.cpp
@@ -58,10 +58,18 @@ namespace Angel3D::Platform::OpenGL
     if(in)
     {
       in.seekg(0, std::ios::end);
-      result.resize(in.tellg());
-      in.seekg(0, std::ios::beg);
-      in.read(&result[0], result.size());
-      in.close();
+			size_t size = in.tellg();
+			if (size != -1)
+			{
+				result.resize(size);
+				in.seekg(0, std::ios::beg);
+				in.read(&result[0], size);
+				in.close();
+			}
+			else
+			{
+        ANGEL3D_CORE_ERROR("Could not read from file '{0}'", f_filePath);
+			}
     }
     else
     {
