557dc3c4f3bcb136feb26c054cae9c7e5b28261e
gowthamkumar12 <gudhey.gowtham2002@gmail.com>
2025-01-02 17:00:53 +0530

    Added timer to Sandbox for basic profiling demo

--- Angel3D/src/ImGui/ImGuiLayer.cpp
+++ Angel3D/src/ImGui/ImGuiLayer.cpp
@@ -81,10 +81,4 @@ namespace Angel3D::ImGuiImpl
       glfwMakeContextCurrent(backup_current_context);
     }
   }
-
-  void ImGuiLayer::OnImGuiRender()
-  {
-    static bool show = true;
-    ImGui::ShowDemoWindow(&show);
-  }
 } // namespace Angel3D
--- Sandbox/src/Sandbox2D.cpp
+++ Sandbox/src/Sandbox2D.cpp
@@ -3,9 +3,45 @@
 #include <imgui.h>
 #include <glm/gtc/matrix_transform.hpp>
 #include <glm/gtc/type_ptr.hpp>
+#include <chrono>
 
 namespace Sandbox
 {
+  template<typename Fn>
+  class Timer
+  {
+    public:
+      Timer(const char* name, Fn&& func)
+        : m_Name(name), m_Func(func), m_Stopped(false)
+      {
+        m_StartTimepoint = std::chrono::high_resolution_clock::now();
+      }
+
+      ~Timer()
+      {
+        if (!m_Stopped)
+          Stop();
+      }
+
+      void Stop()
+      {
+        auto endTimepoint = std::chrono::high_resolution_clock::now();
+        long long start = std::chrono::time_point_cast<std::chrono::microseconds>(m_StartTimepoint).time_since_epoch().count();
+        long long end = std::chrono::time_point_cast<std::chrono::microseconds>(endTimepoint).time_since_epoch().count();
+        m_Stopped = true;
+        float duration = (end - start) * 0.001f;
+        m_Func({ m_Name, duration });
+      }
+
+    private:
+      const char*                                        m_Name;
+      Fn                                                 m_Func;
+      std::chrono::time_point<std::chrono::steady_clock> m_StartTimepoint;
+      bool                                               m_Stopped;
+  };
+
+  #define PROFILE_SCOPE(name) Timer timer##__LINE__(name, [&](ProfileResult profileResult) { m_ProfileResults.push_back(profileResult); })
+
   Sandbox2D::Sandbox2D()
   : Angel3D::Core::Layer("Sandbox3D"),
 	  m_CameraController(1920.0f / 1080.0f)
@@ -22,6 +58,8 @@ namespace Sandbox
 
   void Sandbox2D::OnUpdate(Angel3D::Core::Timestep f_ts)
   {
+    PROFILE_SCOPE("Sandbox2D::OnUpdate");
+
     // Update
 		m_CameraController.OnUpdate(f_ts);
 
@@ -45,6 +83,16 @@ namespace Sandbox
   {
     ImGui::Begin("Settings");
 		ImGui::ColorEdit4("Square Color", glm::value_ptr(m_tileSquareColor));
+
+    for (auto& result : m_ProfileResults)
+    {
+      char label[50];
+      strcpy(label, "%.3fms ");
+      strcat(label, result.f_Name);
+      ImGui::Text(label, result.f_Time);
+    }
+    m_ProfileResults.clear();
+
 		ImGui::End();
   }
 
