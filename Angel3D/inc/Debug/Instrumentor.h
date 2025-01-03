b74a91e1d77c344568ba3491c3d2db08d82b2870
gowthamkumar12 <gudhey.gowtham2002@gmail.com>
2025-01-03 13:24:36 +0530

    Auto deducing an availabe __FUNCSIG__ definition

--- Angel3D/inc/Debug/Instrumentor.h
+++ Angel3D/inc/Debug/Instrumentor.h
@@ -120,10 +120,31 @@ namespace Angel3D::Debug
 
 #define ANGEL3D_PROFILE 1
 #if ANGEL3D_PROFILE
+  // Resolve which function signature macro will be used. Note that this only
+	// is resolved when the (pre)compiler starts, so the syntax highlighting
+	// could mark the wrong one in your editor!
+	#if defined(__GNUC__) || (defined(__MWERKS__) && (__MWERKS__ >= 0x3000)) || (defined(__ICC) && (__ICC >= 600)) || defined(__ghs__) || defined(__clang__)
+		#define ANGEL3D_FUNC_SIG __PRETTY_FUNCTION__
+	#elif defined(__DMC__) && (__DMC__ >= 0x810)
+		#define ANGEL3D_FUNC_SIG __PRETTY_FUNCTION__
+	#elif defined(__FUNCSIG__)
+		#define ANGEL3D_FUNC_SIG __FUNCSIG__
+	#elif (defined(__INTEL_COMPILER) && (__INTEL_COMPILER >= 600)) || (defined(__IBMCPP__) && (__IBMCPP__ >= 500))
+		#define ANGEL3D_FUNC_SIG __FUNCTION__
+	#elif defined(__BORLANDC__) && (__BORLANDC__ >= 0x550)
+		#define ANGEL3D_FUNC_SIG __FUNC__
+	#elif defined(__STDC_VERSION__) && (__STDC_VERSION__ >= 199901)
+		#define ANGEL3D_FUNC_SIG __func__
+	#elif defined(__cplusplus) && (__cplusplus >= 201103)
+		#define ANGEL3D_FUNC_SIG __func__
+	#else
+		#define ANGEL3D_FUNC_SIG "HZ_FUNC_SIG unknown!"
+	#endif
+
 	#define ANGEL3D_PROFILE_BEGIN_SESSION(name, filepath) ::Angel3D::Debug::Instrumentor::Get().BeginSession(name, filepath)
 	#define ANGEL3D_PROFILE_END_SESSION() ::Angel3D::Debug::Instrumentor::Get().EndSession()
 	#define ANGEL3D_PROFILE_SCOPE(name) ::Angel3D::Debug::InstrumentationTimer timer##__LINE__(name);
-	#define ANGEL3D_PROFILE_FUNCTION() ANGEL3D_PROFILE_SCOPE(__FUNCSIG__)
+	#define ANGEL3D_PROFILE_FUNCTION() ANGEL3D_PROFILE_SCOPE(ANGEL3D_FUNC_SIG)
 #else
 	#define ANGEL3D_PROFILE_BEGIN_SESSION(name, filepath)
 	#define ANGEL3D_PROFILE_END_SESSION()
