34dd2268cdc2414878a28e6e190de1c886b0a8d8
gowthamkumar12 <gudhey.gowtham2002@gmail.com>
2025-01-03 15:12:37 +0530

    Instrumentation fixes

--- Angel3D/inc/Debug/Instrumentor.h
+++ Angel3D/inc/Debug/Instrumentor.h
@@ -1,8 +1,13 @@
 #pragma once
 
+#include "Core/Log.h"
+
+#include <algorithm>
 #include <chrono>
 #include <fstream>
+#include <string>
 #include <thread>
+#include <mutex>
 
 namespace Angel3D::Debug
 {
@@ -10,7 +15,7 @@ namespace Angel3D::Debug
 	{
 		std::string Name;
 		long long Start, End;
-		uint32_t ThreadID;
+		std::thread::id ThreadID;
 	};
 
 	struct InstrumentationSession
@@ -21,53 +26,81 @@ namespace Angel3D::Debug
 	class Instrumentor
 	{
     private:
+      std::mutex m_Mutex;
       InstrumentationSession* m_CurrentSession;
       std::ofstream m_OutputStream;
-      int m_ProfileCount;
 
     public:
       Instrumentor()
-        : m_CurrentSession(nullptr), m_ProfileCount(0)
+        : m_CurrentSession(nullptr)
       {
       }
 
       void BeginSession(const std::string& name, const std::string& filepath = "results.json")
       {
+        std::lock_guard lock(m_Mutex);
+        if (m_CurrentSession) {
+          // If there is already a current session, then close it before beginning new one.
+          // Subsequent profiling output meant for the original session will end up in the
+          // newly opened session instead.  That's better than having badly formatted
+          // profiling output.
+          if (Angel3D::Core::Log::GetCoreLogger())
+          { // Edge case: BeginSession() might be before Log::Init()
+            ANGEL3D_CORE_ERROR("Instrumentor::BeginSession('{0}') when session '{1}' already open.", name, m_CurrentSession->Name);
+          }
+          InternalEndSession();
+        }
         m_OutputStream.open(filepath);
-        WriteHeader();
-        m_CurrentSession = new InstrumentationSession{ name };
+        if (m_OutputStream.is_open()) {
+          m_CurrentSession = new InstrumentationSession({name});
+          WriteHeader();
+        } else {
+          if (Angel3D::Core::Log::GetCoreLogger())
+          { // Edge case: BeginSession() might be before Log::Init()
+            ANGEL3D_CORE_ERROR("Instrumentor could not open results file '{0}'.", filepath);
+          }
+        }
       }
 
       void EndSession()
       {
-        WriteFooter();
-        m_OutputStream.close();
-        delete m_CurrentSession;
-        m_CurrentSession = nullptr;
-        m_ProfileCount = 0;
+        std::lock_guard lock(m_Mutex);
+        InternalEndSession();
       }
 
       void WriteProfile(const ProfileResult& result)
       {
-        if (m_ProfileCount++ > 0)
-          m_OutputStream << ",";
+        std::stringstream json;
+
         std::string name = result.Name;
         std::replace(name.begin(), name.end(), '"', '\'');
-        m_OutputStream << "{";
-        m_OutputStream << "\"cat\":\"function\",";
-        m_OutputStream << "\"dur\":" << (result.End - result.Start) << ',';
-        m_OutputStream << "\"name\":\"" << name << "\",";
-        m_OutputStream << "\"ph\":\"X\",";
-        m_OutputStream << "\"pid\":0,";
-        m_OutputStream << "\"tid\":" << result.ThreadID << ",";
-        m_OutputStream << "\"ts\":" << result.Start;
-        m_OutputStream << "}";
-        m_OutputStream.flush();
+
+        json << ",{";
+        json << "\"cat\":\"function\",";
+        json << "\"dur\":" << (result.End - result.Start) << ',';
+        json << "\"name\":\"" << name << "\",";
+        json << "\"ph\":\"X\",";
+        json << "\"pid\":0,";
+        json << "\"tid\":" << result.ThreadID << ",";
+        json << "\"ts\":" << result.Start;
+        json << "}";
+
+        std::lock_guard lock(m_Mutex);
+        if (m_CurrentSession) {
+          m_OutputStream << json.str();
+          m_OutputStream.flush();
+        }
       }
 
+      static Instrumentor& Get() {
+        static Instrumentor instance;
+        return instance;
+      }
+
+    private:
       void WriteHeader()
       {
-        m_OutputStream << "{\"otherData\": {},\"traceEvents\":[";
+        m_OutputStream << "{\"otherData\": {},\"traceEvents\":[{}";
         m_OutputStream.flush();
       }
 
@@ -77,10 +110,15 @@ namespace Angel3D::Debug
         m_OutputStream.flush();
       }
 
-      static Instrumentor& Get()
-      {
-        static Instrumentor instance;
-        return instance;
+      // Note: you must already own lock on m_Mutex before
+      // calling InternalEndSession()
+      void InternalEndSession() {
+        if (m_CurrentSession) {
+          WriteFooter();
+          m_OutputStream.close();
+          delete m_CurrentSession;
+          m_CurrentSession = nullptr;
+        }
       }
 	};
 
@@ -96,18 +134,18 @@ namespace Angel3D::Debug
       ~InstrumentationTimer()
       {
         if (!m_Stopped)
-        {
           Stop();
-        }
       }
 
       void Stop()
       {
         auto endTimepoint = std::chrono::high_resolution_clock::now();
+
         long long start = std::chrono::time_point_cast<std::chrono::microseconds>(m_StartTimepoint).time_since_epoch().count();
         long long end = std::chrono::time_point_cast<std::chrono::microseconds>(endTimepoint).time_since_epoch().count();
-        uint32_t threadID = std::hash<std::thread::id>{}(std::this_thread::get_id());
-        Instrumentor::Get().WriteProfile({ m_Name, start, end, threadID });
+
+        Instrumentor::Get().WriteProfile({ m_Name, start, end, std::this_thread::get_id()});
+
         m_Stopped = true;
       }
 
@@ -138,7 +176,7 @@ namespace Angel3D::Debug
 	#elif defined(__cplusplus) && (__cplusplus >= 201103)
 		#define ANGEL3D_FUNC_SIG __func__
 	#else
-		#define ANGEL3D_FUNC_SIG "HZ_FUNC_SIG unknown!"
+		#define ANGEL3D_FUNC_SIG "ANGEL3D_FUNC_SIG unknown!"
 	#endif
 
 	#define ANGEL3D_PROFILE_BEGIN_SESSION(name, filepath) ::Angel3D::Debug::Instrumentor::Get().BeginSession(name, filepath)
